b42e5508575ce20351a5c4951500016d15975ba9
chulander <bryan.p.chu@gmail.com>
2023-04-23 03:02:26 -0400

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module", jsx: true },
+  plugins: ["react-refresh", "import", "react", "@typescript-eslint"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+    tailwindcss: {
+      whitelist: ["i\\-primary", "i\\-secondary", "i\\-alternate"],
+    },
+  },
+};
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./hooks/ScrollToTop";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ScrollToTop />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
