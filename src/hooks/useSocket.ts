fc6b4201cb5a2cf145909136455294741352dffe
chulander <bryan.p.chu@gmail.com>
2023-04-23 19:12:45 -0400

    working socket

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -7,6 +7,49 @@ export function useSocket() {
   useEffect(() => {
     setSocket(io(import.meta.env.VITE_SERVER));
   }, []);
+  const [isConnected, setIsConnected] = useState(!!socket?.connected);
+
+  useEffect(() => {
+    if (!socket) return;
+    function onConnect() {
+      console.log("connected");
+      setIsConnected(true);
+    }
+
+    function onMessage(data) {
+      console.log("message", data);
+      setIsConnected(true);
+    }
+    function onDisconnect(data) {
+      console.log("disconnected", data);
+      setIsConnected(false);
+    }
+
+    function onComponent(data) {
+      console.log("component to render", data);
+      console.log("what is data", data);
+      if (data === "firstComponent") {
+        // setComponent(ContactUsForm);
+        // setComponent("contact");
+        // Component.current = ContactUsForm;
+      } else {
+        // Component.current = Home;
+        // setComponent("home");
+      }
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("message", onMessage);
+    socket.on("component", onComponent);
+
+    return () => {
+      socket?.off("connect", onConnect);
+      socket?.off("disconnect", onDisconnect);
+      socket?.off("message", onMessage);
+      socket?.off("component", onComponent);
+    };
+  }, [socket]);
   return socket;
 }
 
